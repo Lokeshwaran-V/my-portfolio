fbf6835d0435e5263b502cf9d6f4d6933865bead
Lokesh <sudhavenkat.lokesh+git@gmail.com>
2025-10-15 20:47:46 +0530

    No changes happened in production UI, thus re-rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "aos/dist/aos.css";
+import Aos from "aos";
 
-createRoot(document.getElementById('root')).render(
+Aos.init();
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
